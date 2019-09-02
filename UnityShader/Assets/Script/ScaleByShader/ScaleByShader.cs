1028dc126d38152cc149dae0ac87ea24660e9bd0
eangulee <mbtjay-2004@qq.com>
2019-09-02 17:21:36 +0800

    scale by vs

---
+++ UnityShader/Assets/Script/ScaleByShader/ScaleByShader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ScaleByShader : MonoBehaviour
+{
+    public Renderer[] renderers;
+
+    [SerializeField, SetProperty("scale")]
+    private float _scale = 1f;
+    public float scale
+    {
+        get
+        {
+            return _scale;
+        }
+        set
+        {
+            _scale = value;
+            foreach (var r in renderers)
+            {
+                foreach (var m in r.materials)
+                {
+                    m.SetFloat("_Scale", value);
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderers = this.gameObject.GetComponentsInChildren<Renderer>(true);
+    }
+}
